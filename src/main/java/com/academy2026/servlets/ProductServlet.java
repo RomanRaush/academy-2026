4a600ade0676762d9d4b076ee75a6c3f129d2801
Roman Raush <rn_raush_686@inbox.ru>
2026-06-03 23:34:44 +0300

    Init commit

---
+++ src/main/java/com/academy2026/servlets/ProductServlet.java
@@ -0,0 +1,14 @@
+package com.academy2026.servlets;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ProductServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws SecurityException {
+
+        String method = req.getMethod();
+
+    }
+}
